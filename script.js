1dec9398a7e7d08d8f2e9fec89591578180074c3
gggghale <falquezamiguel@gmail.com>
2020-03-09 12:19:04 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a+b;
+}
+function multiply(a,b){
+    return a+b;
+}
+
+
+function double(num1, num2, callback){
+    return 2* callback(num1, num2);
+    
+}
+
+document.write(double(3,7, add));
+document.write(double(3,7, multiply));
