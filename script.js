823fcb83389475a39df0395f5f49c9b3bf4c8506
gggghale <falquezamiguel@gmail.com>
2020-03-09 12:29:48 +0800

    firstpromise

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-ctr = 0;
-setInterval(function(){
-    ctr += 1;
-    prompt(`${ctr} secs. has elapsed...`)
-}, 1000);
+var promise = new Promise(function(resolve, reject){
+    const name1= "peter";
+    const name1= "peter";
+    if(name1 == name2){
+        resolve();
+    }else{
+        reject();
+    }
+});
+
+promise
+.then(function(){
+    console.log("Successfulli Resolved...")
+})
+.catch(function(){
+    console.log("Failed to resolve...")
+
+})
