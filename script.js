297331ea862e1d2567d164518952f12e68198711
gggghale <falquezamiguel@gmail.com>
2020-03-09 12:25:33 +0800

    set interval

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function add(a,b){
-    return a+b;
-}
-function multiply(a,b){
-    return a+b;
-}
-
-
-function double(num1, num2, callback){
-    return 2* callback(num1, num2);
-    
-}
-
-document.write(double(3,7, add));
-document.write(double(3,7, multiply));
+ctr = 0;
+setInterval(function(){
+    ctr += 1;
+    prompt(`${ctr} secs. has elapsed...`)
+}, 1000);
